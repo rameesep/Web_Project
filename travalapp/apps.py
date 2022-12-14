dd2eb8bd857d95fe0297985d837be29a98cd69e8
Ramees E P <116171985+rameesep@users.noreply.github.com>
2022-12-14 21:43:07 +0530

    Add files via upload

---
+++ travalapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TravalappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'travalapp'
---
+++ travalapp/views.py
@@ -0,0 +1,14 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from . models import place
+
+
+
+# Create your views here.
+
+
+def fun(request):
+    obj=place.objects.all()
+
+    return render(request,"index.html",{"results":obj})
+
